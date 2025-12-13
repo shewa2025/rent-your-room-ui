627cb31ff8c9cadc1648b6bbea3fb761108ac9db
shewangzaw gebeyehu <nags2007@yahoo.com>
2025-12-12 19:09:07 -0500

    Feat: Handle 401 Unauthorized in PublicListingComponent

--- src/app/components/public-listing/public-listing.component.ts
+++ src/app/components/public-listing/public-listing.component.ts
@@ -4,12 +4,15 @@ import { Property } from '../../models/property.model';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { catchError, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-public-listing',
@@ -34,7 +37,12 @@ export class PublicListingComponent implements OnInit {
   currentPage = 0;
   searchControl = new FormControl('');
 
-  constructor(private propertyService: PropertyService) { }
+  constructor(
+    private propertyService: PropertyService,
+    private authService: AuthService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.loadProperties();
@@ -50,6 +58,17 @@ export class PublicListingComponent implements OnInit {
   loadProperties(): void {
     const searchTerm = this.searchControl.value || '';
     this.propertyService.getProperties(this.currentPage, this.pageSize, searchTerm)
+      .pipe(
+        catchError(error => {
+          if (error.status === 401) {
+            if (!this.authService.isLoggedIn) {
+              this.notificationService.showError('You need to log in to view properties.');
+              this.router.navigate(['/login']);
+            }
+          }
+          return of({ data: [], total: 0 }); // Return an empty observable to prevent the app from crashing
+        })
+      )
       .subscribe(response => {
         this.properties = response.data;
         this.totalProperties = response.total;
